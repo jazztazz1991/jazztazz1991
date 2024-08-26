08d138114b37e5e0a99cf8a5fa1f45d6dffd97f0
Cody Chase <cchase1991@gmail.com>
2024-08-26 13:51:41 -0400

    changed navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from '../assets/kevinRushLogo.png';
+import logo from '../assets/cc.png';
 import {
 	FaLinkedin,
 	FaGithub,
-	FaTwitterSquare as FaSqaureXTwittter,
-	FaInstagram,
+	// FaTwitterSquare as FaSqaureXTwittter,
+	// FaInstagram,
 } from 'react-icons/fa';
 
 const Navbar = () => {
 	return (
 		<nav className='mb-20 flex items-center justify-between py-6'>
 			<div className='flex flex-shrink-0 items-center'>
-				<img className='mx-2 w-10' src={logo} alt='Logo' />
+				<img className='mx-2 w-14' src={logo} alt='CC logo' />
 			</div>
 			<div className='m-8 flex item-center justify-center gap-4 text-2xl'>
-				<FaLinkedin />
-				<FaGithub />
-				<FaInstagram />
-				<FaSqaureXTwittter />
+				<a href='https://www.linkedin.com/in/jazztazz/' target='_blank'>
+					<FaLinkedin />
+				</a>
+				<a href='https://github.com/jazztazz1991' target='_blank'>
+					<FaGithub />
+				</a>
 			</div>
 		</nav>
 	);
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,71 +1,63 @@
 import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 7 years of hands-on experience, I have honed my skills in front-end technologies like React and TailwindCSS, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 7 years of hands-on experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep love for technology, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy video games, exploring new technologies, and teaching.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2021 - Present",
+    role: "Teaching Assistant",
+    company: "EdX",
+    description: 'Assisted students in learning web development concepts and technologies, including HTML, CSS, JavaScript, React, and Node.js. Provided feedback on assignments and projects to help students improve their skills. Collaborated with instructors to create engaging and informative course content.',
+    technologies: ["Javascript", "React.js", "PostgreSQL", "mongoDB", "Node.js", "Express.js"],
   },
   {
     year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    role: "Central Grader",
+    company: "EdX",
+    description: `Evaluated and graded student assignments and projects for web development courses. Provided detailed feedback to help students improve their coding skills and understanding of web development concepts. Collaborated with instructors to ensure assignments and projects were aligned with course objectives.`,
+    technologies: ["Javascript", "React.js", "PostgreSQL", "mongoDB", "Node.js", "Express.js"],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    year: "2020 - 2022",
+    role: "Cruise Sales Manager",
+    company: "Expedia Cruises Lake Nona",
+    description: `Managed a team of sales agents and provided training and support to help them achieve their sales targets. Developed and implemented sales strategies to drive revenue growth and increase customer satisfaction. Collaborated with marketing and product teams to launch new products and promotions.`,
+    technologies: [],
   },
   {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "2019 - 2024",
+    role: "Real Estate Agent",
+    company: "Kelly Price & Company",
+    description: `Assisted clients in buying, selling, and renting residential properties. Developed marketing sites to promote high-end properties and attract potential buyers. Negotiated contracts and agreements on behalf of clients to ensure favorable terms and conditions.`,
+    technologies: ["React.js", "mongoDB", "Node.js", "Express.js"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "RsTracker",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "A web application for tracking the progress of a group of players in the popular MMORPG game Runescape. Features include user authentication, task assignment, and progress tracking.",
+    technologies: ["React.js", "TailwindCSS", "Node.js", "MongoDB"],
   },
   {
-    title: "Task Management App",
+    title: "ADHD Task Management App",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking. It is designed with ADHD users in mind, with a focus on gamiphication and ease of use.",
+    technologies: ["React.js", "TailwindCSS", "Node.js", "MongoDB"],
   },
   {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
+    title: "Parsemon",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "A web application for tracking and managing Pokemon collections. Users can create an account, add Pokemon to their collection, and view detailed information about each Pokemon.",
+    technologies: ["Handlebars", "MaterializeCSS", "Node.js", "PostgreSQL"],
   },
 ];
 
